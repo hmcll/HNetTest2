a5f98b6121a6180d52c9140145dd021938fa096e
Michael Huo <hmcll5555@sina.com>
2018-06-01 09:51:43 +0800

    Initial commit

---
+++ Source/HNetTest2/HNetTest2GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HNetTest2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HNETTEST2_API AHNetTest2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
